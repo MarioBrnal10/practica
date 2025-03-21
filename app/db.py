c91013b0100a16f2107e8d53a7028277baeb7b2a
Mario_Brnal10 <145825327+MarioBrnal10@users.noreply.github.com>
2025-03-21 08:55:24 -0600

    m

--- app/db.py
+++ app/db.py
@@ -3,15 +3,18 @@ from flask_mysqldb import MySQL
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # Take environment variables from .env
+load_dotenv()  # Cargar variables de entorno
 
-# MySQL Settings
+# Depuración: Imprimir configuración antes de conectar
+print("MySQL Config:", os.getenv('MYSQL_HOST'), os.getenv('MYSQL_USER'), os.getenv('MYSQL_DB'))
+
+# Configuración de MySQL
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')  # Empty password
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', '')  # Asegurar que toma una cadena vacía
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-# MySQL Connection
+# Conexión con MySQL
 mysql = MySQL(app)
